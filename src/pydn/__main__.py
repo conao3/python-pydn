08739ad3e38db3171da52e0e14f9a22aadfdc4e1
Naoya Yamashita <conao3@gmail.com>
2023-08-22 20:27:39 +0900

    add file argument

--- src/pydn/__main__.py
+++ src/pydn/__main__.py
@@ -1,2 +1,16 @@
+import argparse
+import subprocess
+
+
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('file', help='file to execute')
+
+    return parser.parse_args()
+
+
 def main():
-    print('hello')
+    args = parse_args()
+
+    subprocess.run(['python', args.file])

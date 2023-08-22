fd45dedf9bcd5b8b57d070490a5c404fb1c49567
Naoya Yamashita <conao3@gmail.com>
2023-08-22 21:39:51 +0900

    add creating venv feature

--- src/pydn/__main__.py
+++ src/pydn/__main__.py
@@ -1,4 +1,6 @@
 import argparse
+import hashlib
+import pathlib
 import subprocess
 
 
@@ -13,4 +15,18 @@ def parse_args() -> argparse.Namespace:
 def main():
     args = parse_args()
 
-    subprocess.run(['python', args.file])
+    cachedir = pathlib.Path.home() / '.cache' / 'pydn'
+    venvdir = cachedir / 'venv'
+    venvdir.mkdir(parents=True, exist_ok=True)
+
+    filepath = pathlib.Path(args.file).resolve()
+    prjdir_name = hashlib.sha1(str(filepath).encode()).hexdigest()
+    prjdir = venvdir / prjdir_name
+
+    if not prjdir.exists():
+        print('creating venv...')
+        prjdir.mkdir(parents=True, exist_ok=True)
+        subprocess.run(['python', '-m', 'venv', str(prjdir)])
+
+    print(f'[{prjdir_name}] running...')
+    subprocess.run([str(prjdir / 'bin' / 'python'), args.file])

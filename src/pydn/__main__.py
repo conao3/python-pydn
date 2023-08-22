40fc6e46fc84d0ac4377fbc53ce7237f42a46ae4
Naoya Yamashita <conao3@gmail.com>
2023-08-22 21:51:35 +0900

    add dependency feature

--- src/pydn/__main__.py
+++ src/pydn/__main__.py
@@ -23,10 +23,24 @@ def main():
     prjdir_name = hashlib.sha1(str(filepath).encode()).hexdigest()
     prjdir = venvdir / prjdir_name
 
+    prj_python = str(prjdir / 'bin' / 'python')
+
     if not prjdir.exists():
         print('creating venv...')
         prjdir.mkdir(parents=True, exist_ok=True)
         subprocess.run(['python', '-m', 'venv', str(prjdir)])
+        subprocess.run([prj_python, '-m', 'pip', 'install', '--upgrade', 'pip'])
+
+    with open(args.file) as f:
+        pkgs = [
+            line.split(':')[1].strip()
+            for line in f.readlines()
+            if line.startswith('# pydn:')
+        ]
+
+    if pkgs:
+        print(f'[{prjdir_name}] installing {pkgs}...')
+        subprocess.run([prj_python, '-m', 'pip', 'install', *pkgs])
 
     print(f'[{prjdir_name}] running...')
-    subprocess.run([str(prjdir / 'bin' / 'python'), args.file])
+    subprocess.run([prj_python, args.file])
